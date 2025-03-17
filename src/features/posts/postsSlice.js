59f2d5c7f6324fdd90770d63daddd01aac3b5fe8
Alex Erdei <mralexerdei@yahoo.co.uk>
2025-03-17 12:58:30 +0000

    Show test post

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,15 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const postsSlice = createSlice({
-  name: "posts",
-  initialState: [],
-  reducers: {
-    postsUpdated: (state, action) => {
-      const updatedState = [];
-      action.payload.forEach((post) => updatedState.push(post));
-      return updatedState;
-    },
-  },
+	name: "posts",
+	initialState: [],
+	reducers: {
+		postsUpdated: (state, action) => {
+			//const updatedState = [];
+			action.payload.forEach((post) => {
+				const isPostNew =
+					state.filter((pst) => pst.postID === post.post_id).length === 0;
+				if (isPostNew) {
+					post.postID = post.post_id;
+					delete post.post_id;
+					post.userID = post.user_id;
+					delete post.user_id;
+					post.isPhoto = Boolean(post.isPhoto);
+					post.isYoutube = Boolean(post.isYoutube);
+					post.likes = JSON.parse(post.likes);
+					post.comments = JSON.parse(post.comments);
+					post.timestamp = new Date(post.timestamp).toLocaleString();
+					state.push(post);
+				}
+			});
+			//return updatedState;
+		},
+	},
 });
 
 export const { postsUpdated } = postsSlice.actions;
