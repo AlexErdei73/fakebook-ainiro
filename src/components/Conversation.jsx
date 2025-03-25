34d1b707aa6a231fe19521464c489092c65d5d07
Alex Erdei <mralexerdei@yahoo.co.uk>
2025-03-25 19:27:12 +0000

    Fix bug with sorting dates

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -3,35 +3,35 @@ import DisplayConversation from "./DisplayConversation";
 import { useSelector } from "react-redux";
 
 const Conversation = (props) => {
-  const { recipient } = props;
+	const { recipient } = props;
 
-  const [conversation, setConversation] = useState([]);
+	const [conversation, setConversation] = useState([]);
 
-  const incomingMessages = useSelector((state) => state.incomingMessages);
-  const outgoingMessages = useSelector((state) => state.outgoingMessages);
+	const incomingMessages = useSelector((state) => state.incomingMessages);
+	const outgoingMessages = useSelector((state) => state.outgoingMessages);
 
-  const incoming = incomingMessages.filter(
-    (message) => message.sender === recipient
-  );
-  const outgoing = outgoingMessages.filter(
-    (message) => message.recipient === recipient
-  );
+	const incoming = incomingMessages.filter(
+		(message) => message.sender === recipient
+	);
+	const outgoing = outgoingMessages.filter(
+		(message) => message.recipient === recipient
+	);
 
-  function getConversation(incoming, outgoing) {
-    const conversation = [...incoming, ...outgoing];
-    const sorted = conversation.sort(
-      (msgA, msgB) => new Date(msgA.timestamp) - new Date(msgB.timestamp)
-    );
-    return sorted;
-  }
+	function getConversation(incoming, outgoing) {
+		const conversation = [...incoming, ...outgoing];
+		const sorted = conversation.sort(
+			(msgA, msgB) => msgA.timestamp > msgB.timestamp
+		);
+		return sorted;
+	}
 
-  useEffect(() => {
-    const newConversation = getConversation(incoming, outgoing);
-    if (newConversation.length !== conversation.length)
-      setConversation(newConversation);
-  }, [incoming, outgoing, conversation]);
+	useEffect(() => {
+		const newConversation = getConversation(incoming, outgoing);
+		if (newConversation.length !== conversation.length)
+			setConversation(newConversation);
+	}, [incoming, outgoing, conversation]);
 
-  return <DisplayConversation conversation={conversation} />;
+	return <DisplayConversation conversation={conversation} />;
 };
 
 export default Conversation;
