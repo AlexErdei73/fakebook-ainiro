0b43810830ae975115048bf7abe72dd172403a73
Alex Erdei <mralexerdei@yahoo.co.uk>
2025-03-26 12:13:06 +0000

    Fix bug with isOnline field in usersUpdated function

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,40 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const usersSlice = createSlice({
-	name: "users",
-	initialState: [],
-	reducers: {
-		usersUpdated: (state, action) => {
-			action.payload.forEach((user) => {
-				const isUserNew =
-					state.filter((usr) => usr.userID === user.user_id).length === 0;
-				if (isUserNew) {
-					user.userID = user.user_id;
-					delete user.user_id;
-					user.isOnline = Boolean(user.isOnline);
-					user.isEmailVerified = Boolean(user.isEmailVerified);
-					user.posts = JSON.parse(user.posts);
-					user.photos = JSON.parse(user.photos);
-					delete user.password_hash;
-					state.push(user);
-				} else {
-					const i = state.findIndex((usr) => usr.userID === user.user_id);
-					if (user.isOnline) state[i].isOnline = Boolean(user.isOnline);
-					if (user.isEmailVerified)
-						state[i].isEmailVerified = Boolean(user.isEmailVerified);
-					if (user.posts) state[i].posts = JSON.parse(user.posts);
-					if (user.photos) state[i].photos = JSON.parse(user.photos);
-					if (user.profilePictureURL)
-						state[i].profilePictureURL = user.profilePictureURL;
-					if (user.backgroundPictureURL)
-						state[i].backgroundPictureURL = user.backgroundPictureURL;
-					delete state[i].password_hash;
-					delete state[i].user_id;
-					state[i].userID = user.user_id;
-				}
-			});
-		},
-	},
+  name: "users",
+  initialState: [],
+  reducers: {
+    usersUpdated: (state, action) => {
+      action.payload.forEach((user) => {
+        const isUserNew =
+          state.filter((usr) => usr.userID === user.user_id).length === 0;
+        if (isUserNew) {
+          user.userID = user.user_id;
+          delete user.user_id;
+          user.isOnline = Boolean(user.isOnline);
+          user.isEmailVerified = Boolean(user.isEmailVerified);
+          user.posts = JSON.parse(user.posts);
+          user.photos = JSON.parse(user.photos);
+          delete user.password_hash;
+          state.push(user);
+        } else {
+          const i = state.findIndex((usr) => usr.userID === user.user_id);
+          if (user.isOnline !== undefined)
+            state[i].isOnline = Boolean(user.isOnline);
+          if (user.isEmailVerified)
+            state[i].isEmailVerified = Boolean(user.isEmailVerified);
+          if (user.posts) state[i].posts = JSON.parse(user.posts);
+          if (user.photos) state[i].photos = JSON.parse(user.photos);
+          if (user.profilePictureURL)
+            state[i].profilePictureURL = user.profilePictureURL;
+          if (user.backgroundPictureURL)
+            state[i].backgroundPictureURL = user.backgroundPictureURL;
+          delete state[i].password_hash;
+          delete state[i].user_id;
+          state[i].userID = user.user_id;
+        }
+      });
+    },
+  },
 });
 
 export const { usersUpdated } = usersSlice.actions;
